97e1f00e695c69a6efb7170839097110938c9d8d
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2023-02-20 23:00:59 -0500

    fix: updated working home page with basic component

---
+++ src/components/contactMe.tsx
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react';
+import { email } from '../config';
+
+const ContactMe = () => {
+    return (
+        <div className="text-center py-10 dark:text-white">
+            <h2 className="">What’s Next?</h2>
+            <h2 className="text-5xl py-5">Get In Touch</h2>
+            <p className="py-5">
+                I’m currently looking for any new opportunities and my inbox is
+                always open. If you have a question or just want to say hi, I’ll
+                try my best to get back to you!
+            </p>
+
+            <a className="text-2xl text-teal-600" href={`mailto:${email}`}>
+                Say Hello!
+            </a>
+        </div>
+    );
+};
+
+export default ContactMe;
